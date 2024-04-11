50591a4b5f520e7dbb8fa7b206588af115485a5d
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-04-11 12:14:10 -0500

    Creación de proyecto

---
+++ src/main/java/edu/pe/cibertec/grupo2/DAWI_CL1_2/DawiCl12Application.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.grupo2.DAWI_CL1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl12Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/grupo2/DAWI_CL1_2/DawiCl12ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.grupo2.DAWI_CL1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
